e78540c0f80b2d856d8e8e65d98550f3503b3af8
Sergii Husachenko <gusachenko02@gmail.com>
2021-09-11 20:40:34 +0300

    build mode

--- src/store/client.js
+++ src/store/client.js
@@ -4,7 +4,7 @@ import { setToken, authClear } from "./actions/auth";
 import { userClear } from "./actions/user";
 import Toast from "../components/Toast";
 
-export const API_URL = "http://192.168.0.105:3000/api";
+export const API_URL = "http://localhost:3000/api";
 
 let store;
 
